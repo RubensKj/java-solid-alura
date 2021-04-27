99ed2cc7e4468e17dbbffa595e89df6bb0f07681
rubenskj <rubenskleinschmidtjr@gmail.com>
2021-04-27 20:05:06 -0300

    Creating new validation to salary

--- src/br/com/alura/rh/service/ReajusteService.java
+++ src/br/com/alura/rh/service/ReajusteService.java
@@ -5,6 +5,8 @@ import br.com.alura.rh.model.Funcionario;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 public class ReajusteService {
 
@@ -14,6 +16,15 @@ public class ReajusteService {
 
         if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
             throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
+        }
+
+        LocalDate dataUltimoReajuste = funcionario.getDataUltimoReajuste();
+        LocalDate dataAtual = LocalDate.now();
+
+        long mesesDesdeUltimoReajuste = ChronoUnit.MONTHS.between(dataUltimoReajuste, dataAtual);
+
+        if (mesesDesdeUltimoReajuste < 6) {
+            throw new ValidacaoException("Intervalo entre reajustes deve ser de pelo menos 6 meses!");
         }
 
         BigDecimal salarioReajustado = salarioAtual.add(aumento);
