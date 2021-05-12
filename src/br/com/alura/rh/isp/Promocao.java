35d660e8fea6936cc5293070fc9d5daeeda929b4
rubenskj <rubenskleinschmidtjr@gmail.com>
2021-05-11 21:03:07 -0300

    Using Interface Segregation Principle for Promotion and Anuenio

--- src/br/com/alura/rh/isp/Promocao.java
+++ src/br/com/alura/rh/isp/Promocao.java
@@ -3,7 +3,7 @@ package br.com.alura.rh.isp;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-public class Promocao implements Reajuste {
+public class Promocao implements ReajusteTributavel {
 
     private BigDecimal valor;
     private LocalDate data;
@@ -21,5 +21,10 @@ public class Promocao implements Reajuste {
     @Override
     public LocalDate data() {
         return this.data;
+    }
+
+    @Override
+    public BigDecimal valorImpostoDeRenda() {
+        return this.valor.multiply(new BigDecimal("0.1"));
     }
 }
